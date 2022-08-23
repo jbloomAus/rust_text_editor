24b202feae3b6d7527c22b711c87ae1fdd7515c3
jbloom-md <joseph@massdynamics.com>
2022-08-23 19:14:33 +1000

    rewrite using match

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,31 @@ fn to_ctrl_byte(c: char) -> u8 {
     c as u8 & 0x1f
 }
 
+// This function takes an error and panics, ending the program
+fn die(e: io::Error) {
+    panic!("{}", e);
+}
+
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
     for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let current_char = b as char;
-        println!("{}", current_char);
-        
-        if b == to_ctrl_byte('w') {
-            break;
-        } else if current_char.is_control() {
-            println!("{:?} \r", b);
-        } else {
-            println!("{:?} ({})\r", b, current_char);
+
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("{:?} \r", b);
+                } else {
+                    println!("{:?} ({})\r", b, c);
+                }
+                if b == to_ctrl_byte('c') {
+                    break;
+                }
+            }
+            Err(e) => die(e),
         }
+
+       
     }
 }
