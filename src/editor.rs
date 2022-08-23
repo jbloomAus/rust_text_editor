11b30db7c83a6e2c989956fed7ebd57207f30efd
jbloom-md <joseph@massdynamics.com>
2022-08-23 21:50:58 +1000

    refactor to create terminal crate

--- src/editor.rs
+++ src/editor.rs
@@ -1,58 +1,68 @@
+use crate::Terminal;
 
-use std::io::{self, stdout, Write};
 use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+
 
 pub struct Editor {
     should_quit: bool,
+    terminal: Terminal,
 }
 
 impl Editor {
-    pub fn run(&mut self){
-        let _stdout = stdout().into_raw_mode().unwrap();
-
+    pub fn run(&mut self) {
         loop {
-            if let Err(error) = self.refresh_screen(){
+            if let Err(error) = self.refresh_screen() {
                 die(error);
             }
             if self.should_quit {
                 break;
+            } else {
+                self.draw_rows();
+                print!("{}", termion::cursor::Goto(1, 1))
             }
             if let Err(error) = self.process_keypress() {
                 die(error);
             }
         }
     }
-    
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        Terminal::clear_screen();
+        Terminal::cursor_position(0,0);
+        if self.should_quit {
+            print!("Goodbye.\r");
+        } else {
+            Terminal::cursor_position(0,0);
+        }
+        Terminal::flush()
+    }
+
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-        let pressed_key = read_key()?;
+        let pressed_key = Terminal::read_key();
         match pressed_key {
-            Key::Ctrl('c') => self.should_quit = true,
-            _ => ()
+            Ok(Key::Ctrl('c')) => self.should_quit = true,
+            _ => (),
         }
         Ok(())
     }
 
-    fn refresh_screen(&self) -> Result<(), std::io::Error> {
-        print!("{}", termion::clear::All);
-        io::stdout().flush()
+    fn draw_rows(&self) {
+        for _ in 0..24 {
+            println!("~\r");
+        }
     }
-
     pub fn default() -> Self {
-        Self {should_quit: false}
+        Self {
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to Initialize terminal"),
+        }
     }
 }
 
 // This function takes an error and panics, ending the program
-fn die(e: io::Error) {
+fn die(e: std::io::Error) {
+    print!("{}", termion::clear::All);
     panic!("{}", e);
 }
 
-fn read_key() -> Result<Key, std::io::Error> {
-    loop {
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
-        }
-    }
-}
+
