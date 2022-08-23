89a24fbe1b79ad61ec66bb428c56dcb351dbb03d
jbloom-md <joseph@massdynamics.com>
2022-08-23 18:55:48 +1000

    basic character detection done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
+
 fn main() {
-    println!("Hello, world!");
+    let _stdout = stdout().into_raw_mode().unwrap();
+
+    for b in io::stdin().bytes() {
+        let input = b.as_ref().unwrap();
+        let current_char = *input as char;
+        println!("{}", current_char);
+        
+        if current_char == 'q' {
+            break;
+        } else if current_char.is_control() {
+            println!("{:?} \r", b);
+        } else {
+            println!("{:?} ({})\r", b, current_char);
+        }
+    }
 }
