431529e730d1ebab36da071a3ca97037917a98cf
jbloom-md <joseph@massdynamics.com>
2022-08-23 21:04:45 +1000

    process keypress, flush screen , refactor

---
+++ src/editor.rs
@@ -0,0 +1,58 @@
+
+use std::io::{self, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
+pub struct Editor {
+    should_quit: bool,
+}
+
+impl Editor {
+    pub fn run(&mut self){
+        let _stdout = stdout().into_raw_mode().unwrap();
+
+        loop {
+            if let Err(error) = self.refresh_screen(){
+                die(error);
+            }
+            if self.should_quit {
+                break;
+            }
+            if let Err(error) = self.process_keypress() {
+                die(error);
+            }
+        }
+    }
+    
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
+        let pressed_key = read_key()?;
+        match pressed_key {
+            Key::Ctrl('c') => self.should_quit = true,
+            _ => ()
+        }
+        Ok(())
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        print!("{}", termion::clear::All);
+        io::stdout().flush()
+    }
+
+    pub fn default() -> Self {
+        Self {should_quit: false}
+    }
+}
+
+// This function takes an error and panics, ending the program
+fn die(e: io::Error) {
+    panic!("{}", e);
+}
+
+fn read_key() -> Result<Key, std::io::Error> {
+    loop {
+        if let Some(key) = io::stdin().lock().keys().next() {
+            return key;
+        }
+    }
+}
