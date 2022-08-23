f49af37f45874394d4a699e4a249f7d26b5b4323
jbloom-md <joseph@massdynamics.com>
2022-08-23 19:04:42 +1000

    quit on ctrl w (ctrl q is masked in vscode

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use std::io::{self, stdout, Read};
 use termion::raw::IntoRawMode;
 
+// This function converts a character to a control byte
+fn to_ctrl_byte(c: char) -> u8 {
+    c as u8 & 0x1f
+}
+
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
     for b in io::stdin().bytes() {
-        let input = b.as_ref().unwrap();
-        let current_char = *input as char;
+        let b = b.unwrap();
+        let current_char = b as char;
         println!("{}", current_char);
         
-        if current_char == 'q' {
+        if b == to_ctrl_byte('w') {
             break;
         } else if current_char.is_control() {
             println!("{:?} \r", b);
