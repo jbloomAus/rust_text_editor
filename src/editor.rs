df4c7a552a1bec679be71ac98d272f443abfebae
jbloom-md <joseph@massdynamics.com>
2022-08-25 12:50:18 +1000

    enable to move cursor position

--- src/editor.rs
+++ src/editor.rs
@@ -5,12 +5,30 @@ use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
+
 pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
+    cursor_postion: Position,
 }
 
+
+
 impl Editor {
+
+    pub fn default() -> Self {
+        Self {
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to Initialize terminal"),
+            cursor_postion: Position { x: 0, y: 0 }
+        }
+    }
+
     pub fn run(&mut self) {
         loop {
             if let Err(error) = self.refresh_screen() {
@@ -30,12 +48,12 @@ impl Editor {
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         Terminal::cursor_hide();
-        Terminal::cursor_position(0,0);
+        Terminal::cursor_position(&Position { x: 0, y: 0 });
         if self.should_quit {
             Terminal::clear_screen();
             print!("Goodbye.\r");
         } else {
-            Terminal::cursor_position(0,0);
+            Terminal::cursor_position(&self.cursor_postion);
         }
         Terminal::cursor_show();
         Terminal::flush()
@@ -45,9 +63,32 @@ impl Editor {
         let pressed_key = Terminal::read_key();
         match pressed_key {
             Ok(Key::Ctrl('c')) => self.should_quit = true,
+            Ok(Key::Up) | Ok(Key::Down) | Ok(Key::Left) | Ok(Key::Right) => {
+                self.move_cursor(pressed_key.unwrap());
+            },
             _ => (),
         }
         Ok(())
+    }
+    fn move_cursor(&mut self, key: Key){
+        let Position {mut x, mut y } = self.cursor_postion;
+        let size = self.terminal.size();
+        let height = size.height.saturating_sub(1) as usize;
+        let width = size.width.saturating_sub(1) as usize;
+        match key {
+            Key::Up => y = y.saturating_sub(1),
+            Key::Down => {
+                if y < height {
+                    y = y.saturating_add(1);
+                }}
+            Key::Left => x = x.saturating_sub(1),
+            Key::Right => {
+                if x < width {
+                    x = x.saturating_add(1);
+                }}
+            _ => (),
+        }
+        self.cursor_postion = Position { x, y };
     }
 
     fn draw_welcome_message(&self) {            
@@ -72,13 +113,8 @@ impl Editor {
             }
         }
     }
-    pub fn default() -> Self {
-        Self {
-            should_quit: false,
-            terminal: Terminal::default().expect("Failed to Initialize terminal"),
-        }
-    }
 }
+
 
 // This function takes an error and panics, ending the program
 fn die(e: std::io::Error) {
